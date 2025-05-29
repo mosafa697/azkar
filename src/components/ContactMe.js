c7255c841d93fe3dc96351a5a52f6efa728d48ad
mosafa697 <mosafa697@gmail.com>
2025-05-29 15:37:32 +0300

    fix: Remove toast includes

--- src/components/ContactMe.js
+++ src/components/ContactMe.js
@@ -1,6 +1,4 @@
 import React, { useState } from "react";
-import { ToastContainer, toast } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
 
 const ContactMe = () => {
   const [contactOpen, setContactOpen] = useState(false);
@@ -80,7 +78,6 @@ const ContactMe = () => {
           </button>
         </form>
       )}
-      <ToastContainer rtl position="top-center" />
     </div>
   );
 };
