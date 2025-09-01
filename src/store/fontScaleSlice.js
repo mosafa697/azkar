47db9a45132606e4d99912c07836da8fd618d3e7
mmhafez <Mm.hafez@wakeb.tech>
2025-09-01 16:53:12 +0300

    Revert "test"
    This reverts commit 15370e4675c4d03b7f58b4c042adb31260d33d9b.

--- src/store/fontScaleSlice.js
+++ src/store/fontScaleSlice.js
@@ -28,7 +28,6 @@ const fontScaleSlice = createSlice({
       localStorage.setItem("fontScale", state.value);
     },
   },
-  // todo: add a selector to get the current font scale
 });
 
 export const { incrementFontScale, decrementFontScale } =
